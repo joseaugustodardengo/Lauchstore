ae61cd38ed1a934cdb8aa50e33d4e0e52eb6c6e0
joseaugustodardengo <joseaugusto.dardengo@gmail.com>
2020-05-16 22:47:52 -0300

    Criação da página de busca
    Criado a página de busca e feito uma parte de reaproveitamento da seção de produtos

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -45,6 +45,36 @@ module.exports = {
 
     files(productId) {
         return db.query('SELECT * FROM files WHERE product_id = $1', [productId])        
+    },
+
+    search(params) {
+        const { filter, category } = params
+
+        let query = "",
+            filterQuery = `WHERE`
+
+        if (category) {
+            filterQuery = `${filterQuery}
+            products.category_id = ${category}
+            AND
+            `
+        }
+
+        filterQuery = `
+            ${filterQuery}
+            products.name ilike '%${filter}%'
+            OR products.description ilike '%${filter}%'
+        `
+
+        query = `
+            SELECT products.*,
+                categories.name AS category_name
+            FROM products
+            LEFT JOIN categories ON (categories.id = products.category_id)
+            ${filterQuery}
+        `
+
+        return db.query(query)
     }
 
 
--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,22 @@ const routes = express.Router()
 const multer = require('./app/middlewares/multer')
 const ProductController = require('./app/controllers/ProductController')
 const HomeController = require('./app/controllers/HomeController')
+const SearchController = require('./app/controllers/SearchController')
 
+//Home
 routes.get("/", HomeController.index)
 
+//Search
+routes.get('/products/search', SearchController.index)
+
+//Products
 routes.get('/products/create', ProductController.create)
 routes.get('/products/:id', ProductController.show)
 routes.get('/products/:id/edit', ProductController.edit)
 routes.post('/products', multer.array("photos", 6), ProductController.store)
 routes.put('/products', multer.array("photos", 6), ProductController.update)
 routes.delete('/products',ProductController.destroy)
+
 
 //Alias
 routes.get('/ads/create', function(req,res) {
