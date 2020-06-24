d751531bb5edef72ff9834d33204956dc69e5f40
joseaugustodardengo <joseaugusto.dardengo@gmail.com>
2020-06-24 19:49:27 -0300

    Seed de produtos e arquivos

--- seed.js
+++ seed.js
@@ -1,14 +1,19 @@
 const {hash} = require('bcryptjs')
 const faker = require('faker')
 const User = require('./src/app/models/User')
+const Product = require('./src/app/models/Product')
+const File = require('./src/app/models/File')
 
 let usersIds = []
+let productsIds = []
+let totalProducts = 5
+let totalUsers = 3
 
 async function createUsers() {
     const users = []
     const password = await hash('123',8)
 
-    while(users.length < 3) {
+    while(users.length < totalUsers) {
 
         users.push({
             name: faker.name.firstName(),
@@ -24,4 +29,43 @@ async function createUsers() {
     usersIds = await Promise.all(usersPromise)
 }
 
-createUsers()
+async function createProducts() {
+    let products = []
+
+    while(products.length < totalProducts) {
+        products.push({
+            category_id: Math.ceil(Math.random() * 2),
+            user_id: usersIds[Math.floor(Math.random() * totalUsers)],
+            name: faker.name.title(),
+            description: faker.lorem.paragraph(Math.ceil(Math.random() * 2)),
+            old_price: faker.random.number(99999),
+            price: faker.random.number(99999),
+            quantity: faker.random.number(99),
+            status: Math.round(Math.random()) 
+        })
+    }
+
+    const productsPromise = products.map(product => Product.create(product))
+    productsIds = await Promise.all(productsPromise)
+
+    let files = []
+
+    while(files.length < 10) {
+        files.push({
+            name: faker.image.image(),
+            path: `public/images/placeholder.png`,
+            product_id: productsIds[Math.floor(Math.random() * totalProducts)],
+        })
+    }
+
+    const filesPromise = files.map(file => File.create(file))
+    await Promise.all(filesPromise)
+    
+}
+
+async function init() {
+    await createUsers(),
+    await createProducts()
+}
+
+init()
