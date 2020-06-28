44feab22a1551f7c52062bb28da397814651b505
joseaugustodardengo <joseaugusto.dardengo@gmail.com>
2020-06-27 21:04:15 -0300

    Pedido de compra e mensagem pós pedido

--- src/lib/mailer.js
+++ src/lib/mailer.js
@@ -4,7 +4,7 @@ module.exports = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
     port: 2525,
     auth: {
-      user: "9ba7533dbf3902",
-      pass: "d55a5df3dd3cb8"
+      user: "cc959135f92c7c",
+      pass: "46f776a09b298d"
     }
   });  
