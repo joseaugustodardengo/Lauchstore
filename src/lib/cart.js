2bfff6b454be7964d9fdc2db58dfd0b9d985c9ed
joseaugustodardengo <joseaugusto.dardengo@gmail.com>
2020-07-01 18:52:36 -0300

    Removendo item do carrinho

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -47,7 +47,28 @@ const Cart = {
         return this
     },
 
-    removeOne(productId) {},
+    removeOne(productId) {
+        let inCart = this.items.find(item => item.product.id == productId)
+
+        if(!inCart) return this
+
+        inCart.quantity--
+        inCart.price = inCart.product.price * inCart.quantity
+        inCart.formattedPrice = formatPrice(inCart.price)
+
+        this.total.quantity--
+        this.total.price -= inCart.product.price
+        this.total.formattedPrice = formatPrice(this.total.price)
+        
+        if(inCart.quantity < 1) {
+            this.items = this.items.filter(item => item.product.id != inCart.product.id)
+
+            return this
+        }
+
+        return this
+
+    },
 
     delete(productId) {}
 }
@@ -60,7 +81,7 @@ const product = {
 
 const product2 = {
     id: 2,
-    price: 299,
+    price: 229,
     quantity: 1
 }
 
@@ -76,8 +97,13 @@ console.log('add third cart item')
 oldCart = Cart.init(oldCart).addOne(product2)
 console.log(oldCart)
 
-console.log('add last cart item')
-oldCart = Cart.init(oldCart).addOne(product)
+console.log('remove one item')
+oldCart = Cart.init(oldCart).removeOne(product.id)
 console.log(oldCart)
+
+console.log('remove one item')
+oldCart = Cart.init(oldCart).removeOne(product.id)
+console.log(oldCart)
+
 
 module.exports = Cart
