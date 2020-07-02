e5960fe8156f6cae3510d129575285e8d41ecbaf
joseaugustodardengo <joseaugusto.dardengo@gmail.com>
2020-07-02 14:04:06 -0300

    Funcionalidade de adicionar ao carrinho

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -1,20 +1,37 @@
 const Cart = require('../../lib/cart')
 const LoadProductService = require('../services/LoadProductService')
+const { addOne } = require('../../lib/cart')
 
 module.exports = {
     async index(req, res) {
-        try {
-            const product = await LoadProductService.load('product', {where : {id: 1}})
+        try {           
 
             let { cart } = req.session
 
-            cart = Cart.init(cart).addOne(product)
+            cart = Cart.init(cart)
 
             return res.render("cart/index", { cart })
         } catch (error) {
             console.error(error)
         }
 
+    },
+
+    async addOne(req,res) {
+        try {
+            const {id} = req.params
+
+            const product = await LoadProductService.load('product', {where: {id}})
+
+            let { cart } = req.session
+
+            req.session.cart = Cart.init(cart).addOne(product)
+
+            return res.redirect('/cart')
+
+        } catch (error) {
+            console.error(error)
+        }
     }
 
 }
