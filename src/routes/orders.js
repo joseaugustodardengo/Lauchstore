730d8f09e8aab353017550f49d3830ff246d5bda
joseaugustodardengo <joseaugusto.dardengo@gmail.com>
2020-07-02 22:32:59 -0300

    Menu para editar pedido

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -6,8 +6,8 @@ const { onlyUsers } = require('../app/middlewares/session')
 
 
 routes.get('/', onlyUsers, OrderController.index)
-routes.get('/:id', onlyUsers, OrderController.show)
 routes.get('/sales', onlyUsers, OrderController.sales)
+routes.get('/:id', onlyUsers, OrderController.show)
 routes.post('/', onlyUsers, OrderController.store)
 
 
